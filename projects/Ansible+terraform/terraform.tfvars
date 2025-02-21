f8175c2b45f4d631267ef6428672b76455cee117
sawsan.aly <sawsan.aly@cmeoffshore.com>
2025-02-22 01:47:00 +0200

    add projet

--- projects/Ansible+terraform/terraform.tfvars
+++ projects/Ansible+terraform/terraform.tfvars
@@ -1,6 +1,6 @@
 nstance_name = "Test-instance"
-instance_type = "t2.medium"
-subnet_id = "subnet-0164395797ba54f93"
-ami_id = "ami-0f58b397bc5c1f2e8"
-number_of_instances = 2
-ami_key_pair_name = "DevOps"
+instance_type = "t3.micro"
+subnet_id = "subnet-04ffbd669cd37a2ee"
+ami_id = "ami-09a9858973b288bdd"
+number_of_instances = 1
+ami_key_pair_name = "Devops"
--- projects/Ansible+terraform/variables.tf
+++ projects/Ansible+terraform/variables.tf
@@ -4,15 +4,15 @@ default = "Test-instance"
 }
 variable "instance_type" {
 description = "Type of instance to be created"
-default = "t2.micro"
+default = "t3.micro"
 }
 variable "subnet_id" {
 description = "The VPC subnet the instance(s) will be created in"
-default = "subnet-0164395797ba54f93"
+default = "subnet-04ffbd669cd37a2ee"
 }
 variable "ami_id" {
 description = "The AMI to use"
-default = "ami-08e5424edfe926b43"
+default = "ami-09a9858973b288bdd"
 }
 variable "number_of_instances" {
 description = "Number of instances to be created"
@@ -21,4 +21,9 @@ default = 1
 variable "ami_key_pair_name" {
 description = "Key pair name for the instances"
 default = "DevOps"
+}
+variable "region" {
+    description = "region"
+    default = "eu-north-1"
+  
 }
